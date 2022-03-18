0721324ca879551f2e6989883aa5aa97b9fcc2ef
Marvin Kamphausen <marvin.kamphausen@icloud.com>
2022-03-18 20:27:17 +0100

    updated search styles; search buttons change mode

--- anime-map-project/src/components/Search.js
+++ anime-map-project/src/components/Search.js
@@ -14,21 +14,23 @@ const Search = ({animeCollection, filter, updateFilterBuildings, updatefilterNat
 
     const [natureButton, setNatureButton ] = React.useState(true);
     const [buildingButton, setBuildingButton ] = React.useState(true);
-    const [natureMode, setNatureMode] = React.useState(true);
-    const [buildingMode, setBuildingMode] = React.useState(true);
-    const bla = filter.Buildings;
-    const bli = filter.Nature;
-    const blub = filter.AnimeID;
+    const [natureMode, setNatureMode] = React.useState('primary');
+    const [buildingMode, setBuildingMode] = React.useState('primary');
 
     const natureButtonHandler = () => {
         console.log("NATURE!!")
         if(natureButton){
+            //will erase filter
             updatefilterNature( filter.buildings, false, filter.animeID)
             setNatureButton(false)
+            setNatureMode('primary')
         }else{
+            //set filter
             updatefilterNature(filter.buildings, true, filter.animeID)
             setNatureButton(true)
+            setNatureMode('secondary')
         }
+        console.log(natureMode)
         console.log(natureButton)
         // component.forceUpdate()
     }
@@ -36,12 +38,17 @@ const Search = ({animeCollection, filter, updateFilterBuildings, updatefilterNat
     const buildingButtonHandler = () => {
         console.log("BUILDINGS!!")
         if(buildingButton){
+            //will erase filter
             updateFilterBuildings(false, filter.nature, filter.animeID)
             setBuildingButton(false)
+            setBuildingMode('primary')
         }else{
+            //set filter
             updateFilterBuildings(true, filter.nature, filter.animeID)
             setBuildingButton(true)
+            setBuildingMode('secondary')
         }
+        console.log(buildingMode)
         console.log(buildingButton)
         // component.forceUpdate()
     }
@@ -55,19 +62,21 @@ const Search = ({animeCollection, filter, updateFilterBuildings, updatefilterNat
 
     return (
         <>
-            <div>
-                <Button onClick={natureButtonHandler}>natural sites</Button>
-                <Button onClick={buildingButtonHandler}>buildings</Button>
-            </div>
-                <div>
-                <span>Anime für Filter auswählen:</span>
-                <select name="animeSelect" id="animeSelect" onChange={animeSelectHandler}>
-                    <option value='' key='default' defaultValue>kein Filter</option>
-                    {animeCollection.map((anime) => (
-                        <option value={anime.id} key={anime.id}>{anime.title}</option>
-                    )
-                    )};
-                </select>
+            <div className="searchContainer">
+                <div className="searchButtonContainer">
+                    <Button className="searchButton" variant={natureMode} onClick={natureButtonHandler}>natural sites</Button>
+                    <Button className="searchButton" variant={buildingMode} onClick={buildingButtonHandler}>buildings</Button>
+                </div>
+                    <div>
+                    <span>Anime für Filter auswählen:</span>
+                    <select name="animeSelect" id="animeSelect" onChange={animeSelectHandler}>
+                        <option value='' key='default' defaultValue>kein Filter</option>
+                        {animeCollection.map((anime) => (
+                            <option value={anime.id} key={anime.id}>{anime.title}</option>
+                        )
+                        )};
+                    </select>
+                </div>
             </div>
         </>
     )
