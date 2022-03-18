d0a15826c5a713fafc1a2c5200611fb80061a121
Marvin Kamphausen <marvin.kamphausen@icloud.com>
2022-03-18 12:46:13 +0100

    update header

--- anime-map-project/src/components/Header.js
+++ anime-map-project/src/components/Header.js
@@ -4,20 +4,12 @@ import React from 'react';
 import Sidebar from './Sidebar';
 //import bootstrap & styles
 import { Navbar, Container, Nav  } from 'react-bootstrap';
-import logo from '../logo.svg'
 
 const Header = ({ filter, places, animeCollection, updateFilterBuildings, updatefilterNature, updatefilterAnimeID })  => (
   <Navbar className='header' expand="md">
     <Container fluid>
     <div className="container">
       <Navbar.Brand href="#">        
-        <img
-          alt=""
-          src={logo}
-          width="30"
-          height="30"
-          className="d-inline-block align-top"
-        />{' '}
         Anime Map Project
       </Navbar.Brand>
         <Sidebar
